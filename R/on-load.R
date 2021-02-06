750b56fd738e548083cd567372a82d99e931748d
Chad Goymer <chad.goymer@gmail.com>
2021-02-06 11:43:26 +0000

    Only set options on-load if they do not already exist

--- R/on-load.R
+++ R/on-load.R
@@ -1,8 +1,9 @@
 .onLoad <- function(libname, pkgname) {
+
   # Read configuration file
   if (
     Sys.getenv("GITHAPI_CONFIG") != "" &&
-      file.exists(Sys.getenv("GITHAPI_CONFIG"))
+    file.exists(Sys.getenv("GITHAPI_CONFIG"))
   ) {
     config_path <- Sys.getenv("GITHAPI_CONFIG")
   } else {
@@ -29,63 +30,73 @@
     app_cfg <- config[names(config) != "github"][[1]]
   }
 
-  # Set github token
-  tokens <- c(
-    Sys.getenv("GITHAPI_TOKEN"),
-    Sys.getenv("GITHUB_PAT"),
-    Sys.getenv("GITHUB_TOKEN")
-  )
-
-  if (any(tokens != "")) {
-    gh_cfg$token <- tokens[tokens != ""][[1]]
-  }
-
   # Override github API URL
-  api <- c(
-    Sys.getenv("GITHUB_API"),
-    Sys.getenv("GITHUB_API_URL")
-  )
+  if (is.null(getOption("github.api"))) {
+    if (Sys.getenv("GITHUB_API_URL") != "") {
+      gh_cfg$api <- Sys.getenv("GITHUB_API_URL")
+    }
 
-  if (any(api != "")) {
-    gh_cfg$api <- api[api != ""][[1]]
+    options(github.api = gh_cfg$api)
   }
 
   # Override github OAuth URL
-  if (Sys.getenv("GITHUB_OAUTH") != "") {
-    gh_cfg$oauth <- Sys.getenv("GITHUB_OAUTH")
+  if (is.null(getOption("github.oauth"))) {
+    if (Sys.getenv("GITHUB_OAUTH") != "") {
+      gh_cfg$oauth <- Sys.getenv("GITHUB_OAUTH")
+    }
+
+    options(github.oauth = gh_cfg$oauth)
   }
 
   # Override github proxy URL
-  if (Sys.getenv("GITHUB_PROXY") != "") {
-    gh_cfg$proxy <- Sys.getenv("GITHUB_PROXY")
+  if (is.null(getOption("github.proxy"))) {
+    if (Sys.getenv("GITHUB_PROXY") != "") {
+      gh_cfg$proxy <- Sys.getenv("GITHUB_PROXY")
+    }
+
+    options(github.proxy = gh_cfg$proxy)
+  }
+
+  # Set github token
+  if (is.null(getOption("github.token"))) {
+    tokens <- c(
+      Sys.getenv("GITHAPI_TOKEN"),
+      Sys.getenv("GITHUB_PAT"),
+      Sys.getenv("GITHUB_TOKEN")
+    )
+
+    if (any(tokens != "")) {
+      gh_cfg$token <- tokens[tokens != ""][[1]]
+    }
+
+    options(github.token = gh_cfg$token)
   }
 
   # Override githapi application ID
-  if (Sys.getenv("GITHAPI_ID") != "") {
-    app_cfg$id <- Sys.getenv("GITHAPI_ID")
+  if (is.null(getOption("githapi.id"))) {
+    if (Sys.getenv("GITHAPI_ID") != "") {
+      app_cfg$id <- Sys.getenv("GITHAPI_ID")
+    }
+
+    options(githapi.id = app_cfg$id)
   }
 
   # Override githapi application secret
-  if (Sys.getenv("GITHAPI_SECRET") != "") {
-    app_cfg$secret <- Sys.getenv("GITHAPI_SECRET")
+  if (is.null(getOption("githapi.secret"))) {
+    if (Sys.getenv("GITHAPI_SECRET") != "") {
+      app_cfg$secret <- Sys.getenv("GITHAPI_SECRET")
+    }
+
+    options(githapi.secret = app_cfg$secret)
   }
 
   # Override githapi application token cache location
-  if (Sys.getenv("GITHAPI_CACHE") != "") {
-    app_cfg$cache <- Sys.getenv("GITHAPI_CACHE")
+  if (is.null(getOption("githapi.cache"))) {
+    if (Sys.getenv("GITHAPI_CACHE") != "") {
+      app_cfg$cache <- Sys.getenv("GITHAPI_CACHE")
+    }
+
+    options(githapi.cache = app_cfg$cache)
   }
 
-  # Set R options
-  options(
-    # GitHub API
-    github.api       = gh_cfg$api,        # The URL of the GitHub API
-    github.oauth     = gh_cfg$oauth,      # The URL for GitHub OAuth
-    github.token     = gh_cfg$token,      # The GitHub token
-    github.proxy     = gh_cfg$proxy,      # The proxy to use to access GitHub
-
-    # githapi application
-    githapi.id       = app_cfg$id,        # The GitHub application ID
-    githapi.secret   = app_cfg$secret,    # The GitHub application secret
-    githapi.cache    = app_cfg$cache      # Location for cached token
-  )
 }
