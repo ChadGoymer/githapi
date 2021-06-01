89f25ea7d7d2a0938e8dde815e9b7d474ccf6a4d
Chad Goymer <chad.goymer@gmail.com>
2021-06-01 17:51:06 +0100

    Removed username from test-users

--- tests/testthat/test-users.R
+++ tests/testthat/test-users.R
@@ -1,11 +1,40 @@
-context("users")
+# SETUP ------------------------------------------------------------------------
+
+suffix <- sample(letters, 10, replace = TRUE) %>% str_c(collapse = "")
+
+suppressMessages({
+
+  user <- view_user()
+
+  org <- view_organizations(user = NULL, n_max = 10) %>%
+    arrange(.data$login) %>%
+    slice(1)
+
+  if (nrow(org) == 1) {
+    create_team(
+      name        = str_c("test-users-", suffix),
+      org         = org$login,
+      description = "This is a team to test users"
+    )
+  }
+
+})
+
+teardown(suppressMessages({
+
+  try(
+    delete_team(str_c("test-users-", suffix), org = org$login),
+    silent = TRUE
+  )
+
+}))
 
 
 # TEST: update_user ------------------------------------------------------------
 
 test_that("update_user changes the user's properties", {
 
-  original_user <- view_user("ChadGoymer")
+  original_user <- view_user(user$login)
 
   on.exit({
     update_user(
@@ -48,7 +77,7 @@ test_that("update_user changes the user's properties", {
     )
   )
 
-  expect_identical(updated_user$login, "ChadGoymer")
+  expect_identical(updated_user$login, user$login)
   expect_identical(updated_user$name, "Bob")
   expect_identical(updated_user$location, "Nowhere")
   expect_identical(updated_user$hireable, TRUE)
@@ -59,38 +88,6 @@ test_that("update_user changes the user's properties", {
 # TEST: view_users -------------------------------------------------------------
 
 test_that("view_users returns a tibble summarising the users", {
-
-  org_users <- view_users(org = "HairyCoos", n_max = 10)
-
-  expect_is(org_users, "tbl")
-  expect_identical(attr(org_users, "status"), 200L)
-  expect_identical(
-    map_chr(org_users, ~ class(.)[[1]]),
-    c(
-      id         = "integer",
-      login      = "character",
-      site_admin = "logical",
-      html_url   = "character"
-    )
-  )
-
-  expect_true("ChadGoymer" %in% org_users$login)
-
-  team_users <- view_users(org = "HairyCoos", team = "HeadCoos", n_max = 10)
-
-  expect_is(team_users, "tbl")
-  expect_identical(attr(team_users, "status"), 200L)
-  expect_identical(
-    map_chr(team_users, ~ class(.)[[1]]),
-    c(
-      id         = "integer",
-      login      = "character",
-      site_admin = "logical",
-      html_url   = "character"
-    )
-  )
-
-  expect_true("ChadGoymer" %in% team_users$login)
 
   all_users <- view_users(n_max = 10)
 
@@ -106,7 +103,47 @@ test_that("view_users returns a tibble summarising the users", {
     )
   )
 
-  admin_users <- view_users(org = "HairyCoos", role = "admin", n_max = 10)
+  skip_if(
+    length(org$login) != 1,
+    "Authenticated user is not a member of an organization"
+  )
+
+  org_users <- view_users(org = org$login)
+
+  expect_is(org_users, "tbl")
+  expect_identical(attr(org_users, "status"), 200L)
+  expect_identical(
+    map_chr(org_users, ~ class(.)[[1]]),
+    c(
+      id         = "integer",
+      login      = "character",
+      site_admin = "logical",
+      html_url   = "character"
+    )
+  )
+
+  expect_true(user$login %in% org_users$login)
+
+  team_users <- view_users(
+    org = org$login,
+    team = str_c("test-users-", suffix)
+  )
+
+  expect_is(team_users, "tbl")
+  expect_identical(attr(team_users, "status"), 200L)
+  expect_identical(
+    map_chr(team_users, ~ class(.)[[1]]),
+    c(
+      id         = "integer",
+      login      = "character",
+      site_admin = "logical",
+      html_url   = "character"
+    )
+  )
+
+  expect_true(user$login %in% team_users$login)
+
+  admin_users <- view_users(org = org$login, role = "admin")
 
   expect_is(admin_users, "tbl")
   expect_identical(attr(admin_users, "status"), 200L)
@@ -120,7 +157,7 @@ test_that("view_users returns a tibble summarising the users", {
     )
   )
 
-  expect_true("ChadGoymer" %in% admin_users$login)
+  expect_true(user$login %in% admin_users$login)
   expect_true(nrow(admin_users) < nrow(org_users))
 
 })
@@ -129,8 +166,6 @@ test_that("view_users returns a tibble summarising the users", {
 # TEST: view_user --------------------------------------------------------------
 
 test_that("view_user returns a list of user properties", {
-
-  user <- view_user("ChadGoymer")
 
   expect_is(user, "list")
   expect_identical(attr(user, "status"), 200L)
@@ -151,14 +186,13 @@ test_that("view_user returns a list of user properties", {
     )
   )
 
-  expect_identical(user$login, "ChadGoymer")
 
-  auth_user <- view_user()
+  named_user <- view_user(user$login)
 
-  expect_is(auth_user, "list")
-  expect_identical(attr(auth_user, "status"), 200L)
+  expect_is(named_user, "list")
+  expect_identical(attr(named_user, "status"), 200L)
   expect_identical(
-    map_chr(auth_user, ~ class(.)[[1]]),
+    map_chr(named_user, ~ class(.)[[1]]),
     c(
       id         = "integer",
       login      = "character",
@@ -174,7 +208,7 @@ test_that("view_user returns a list of user properties", {
     )
   )
 
-  expect_identical(auth_user$login, "ChadGoymer")
+  expect_identical(named_user$login, user$login)
 
 })
 
@@ -185,16 +219,20 @@ test_that("browse_user opens the user's page in the browser", {
 
   skip_if(!interactive(), "browse_user must be tested manually")
 
-  user <- browse_user("ChadGoymer")
-
-  expect_is(user, "character")
-  expect_identical(attr(user, "status"), 200L)
-  expect_identical(as.character(user), "https://github.com/ChadGoymer")
-
   auth_user <- browse_user()
+
+  base_url <- getOption("github.oauth") %>%
+    str_remove("login/oauth") %>%
+    str_c(user$login)
 
   expect_is(auth_user, "character")
   expect_identical(attr(auth_user, "status"), 200L)
-  expect_identical(as.character(auth_user), "https://github.com/ChadGoymer")
+  expect_identical(as.character(auth_user), base_url)
+
+  named_user <- browse_user(user$login)
+
+  expect_is(named_user, "character")
+  expect_identical(attr(named_user, "status"), 200L)
+  expect_identical(as.character(named_user), base_url)
 
 })
