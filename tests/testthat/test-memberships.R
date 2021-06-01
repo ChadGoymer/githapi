1d3cb758f242916c142d3d7f4e972661a1cd34b7
Chad Goymer <chad.goymer@gmail.com>
2021-06-01 12:06:02 +0100

    Removed username from test-memberships

--- tests/testthat/test-memberships.R
+++ tests/testthat/test-memberships.R
@@ -1,11 +1,37 @@
-context("memberships")
+# SETUP ------------------------------------------------------------------------
 
+suppressMessages({
+
+  user_1 <- view_user()
+  user_2 <- tryCatch(
+    view_user("ChadGoymer2"),
+    error = function(e) list(login = character())
+  )
+
+  org <- view_organizations(user = NULL, n_max = 10) %>%
+    arrange(.data$login) %>%
+    slice(1)
+
+  team <- view_teams(org = org$login) %>%
+    arrange(.data$name) %>%
+    slice(1)
+
+})
 
 # TEST: update_membership ------------------------------------------------------
 
 test_that("update_membership returns a list of membership properties", {
 
-  org_membership <- update_membership("ChadGoymer2", "HairyCoos")
+  skip_if(
+    length(user_2$login) != 1,
+    "Test user does not exist"
+  )
+  skip_if(
+    length(org$login) != 1,
+    "Authenticated user is not a member of an organization"
+  )
+
+  org_membership <- update_membership(user_2$login, org$login)
 
   expect_is(org_membership, "list")
   expect_identical(attr(org_membership, "status"), 200L)
@@ -19,14 +45,14 @@ test_that("update_membership returns a list of membership properties", {
     )
   )
 
-  expect_identical(org_membership$user, "ChadGoymer2")
-  expect_identical(org_membership$organization, "HairyCoos")
+  expect_identical(org_membership$user, user_2$login)
+  expect_identical(org_membership$organization, org$login)
   expect_identical(org_membership$role, "member")
   expect_identical(org_membership$state, "pending")
 
   org_role_membership <- update_membership(
-    user = "ChadGoymer2",
-    org  = "HairyCoos",
+    user = user_2$login,
+    org  = org$login,
     role = "admin"
   )
 
@@ -42,13 +68,17 @@ test_that("update_membership returns a list of membership properties", {
     )
   )
 
-  expect_identical(org_role_membership$user, "ChadGoymer2")
-  expect_identical(org_role_membership$organization, "HairyCoos")
+  expect_identical(org_role_membership$user, user_2$login)
+  expect_identical(org_role_membership$organization, org$login)
   expect_identical(org_role_membership$role, "admin")
   expect_identical(org_role_membership$state, "pending")
 
+  skip_if(
+    length(team$name) != 1,
+    "Organization does not have any teams defined"
+  )
 
-  team_membership <- update_membership("ChadGoymer2", "HairyCoos", "HeadCoos")
+  team_membership <- update_membership(user_2$login, org$login, team$name)
 
   expect_is(team_membership, "list")
   expect_identical(attr(team_membership, "status"), 200L)
@@ -63,16 +93,16 @@ test_that("update_membership returns a list of membership properties", {
     )
   )
 
-  expect_identical(team_membership$user, "ChadGoymer2")
-  expect_identical(team_membership$organization, "HairyCoos")
-  expect_identical(team_membership$team, "HeadCoos")
+  expect_identical(team_membership$user, user_2$login)
+  expect_identical(team_membership$organization, org$login)
+  expect_identical(team_membership$team, team$name)
   expect_identical(team_membership$role, "member")
   expect_identical(team_membership$state, "pending")
 
   team_role_membership <- update_membership(
-    user = "ChadGoymer2",
-    org  = "HairyCoos",
-    team = "HeadCoos",
+    user = user_2$login,
+    org  = org$login,
+    team = team$name,
     role = "maintainer"
   )
 
@@ -89,9 +119,9 @@ test_that("update_membership returns a list of membership properties", {
     )
   )
 
-  expect_identical(team_role_membership$user, "ChadGoymer2")
-  expect_identical(team_role_membership$organization, "HairyCoos")
-  expect_identical(team_role_membership$team, "HeadCoos")
+  expect_identical(team_role_membership$user, user_2$login)
+  expect_identical(team_role_membership$organization, org$login)
+  expect_identical(team_role_membership$team, team$name)
   expect_identical(team_role_membership$role, "maintainer")
   expect_identical(team_role_membership$state, "pending")
 
@@ -116,10 +146,10 @@ test_that("view_memberships returns a tibble summarising the memberships", {
     )
   )
 
-  expect_true("HairyCoos" %in% memberships$organization)
+  expect_true(org$login %in% memberships$organization)
 
-  hairy_coos <- filter(memberships, .data$organization == "HairyCoos")
-  expect_identical(hairy_coos$user, "ChadGoymer")
+  hairy_coos <- filter(memberships, .data$organization == org$login)
+  expect_identical(hairy_coos$user, user_1$login)
   expect_identical(hairy_coos$state, "active")
   expect_identical(hairy_coos$role, "admin")
 
@@ -137,12 +167,12 @@ test_that("view_memberships returns a tibble summarising the memberships", {
     )
   )
 
-  expect_true("HairyCoos" %in% active_memberships$organization)
+  expect_true(org$login %in% active_memberships$organization)
 
   active_hairy_coos <- active_memberships %>%
-    filter(.data$organization == "HairyCoos")
+    filter(.data$organization == org$login)
 
-  expect_identical(active_hairy_coos$user, "ChadGoymer")
+  expect_identical(active_hairy_coos$user, user_1$login)
   expect_identical(active_hairy_coos$state, "active")
   expect_identical(active_hairy_coos$role, "admin")
 
@@ -167,7 +197,12 @@ test_that("view_memberships returns a tibble summarising the memberships", {
 
 test_that("view_membership returns a list of membership properties", {
 
-  user_membership <- view_membership("ChadGoymer2", "HairyCoos")
+  skip_if(
+    length(org$login) != 1,
+    "Authenticated user is not a member of an organization"
+  )
+
+  user_membership <- view_membership(user_1$login, org$login)
 
   expect_is(user_membership, "list")
   expect_identical(attr(user_membership, "status"), 200L)
@@ -181,12 +216,17 @@ test_that("view_membership returns a list of membership properties", {
     )
   )
 
-  expect_identical(user_membership$user, "ChadGoymer2")
-  expect_identical(user_membership$organization, "HairyCoos")
+  expect_identical(user_membership$user, user_1$login)
+  expect_identical(user_membership$organization, org$login)
   expect_identical(user_membership$role, "admin")
-  expect_identical(user_membership$state, "pending")
+  expect_identical(user_membership$state, "active")
 
-  team_membership <- view_membership("ChadGoymer2", "HairyCoos", "HeadCoos")
+  skip_if(
+    length(team$name) != 1,
+    "Organization does not have any teams defined"
+  )
+
+  team_membership <- view_membership(user_1$login, org$login, team$name)
 
   expect_is(team_membership, "list")
   expect_identical(attr(team_membership, "status"), 200L)
@@ -201,11 +241,11 @@ test_that("view_membership returns a list of membership properties", {
     )
   )
 
-  expect_identical(team_membership$user, "ChadGoymer2")
-  expect_identical(team_membership$organization, "HairyCoos")
-  expect_identical(team_membership$team, "HeadCoos")
+  expect_identical(team_membership$user, user_1$login)
+  expect_identical(team_membership$organization, org$login)
+  expect_identical(team_membership$team, team$name)
   expect_identical(team_membership$role, "maintainer")
-  expect_identical(team_membership$state, "pending")
+  expect_identical(team_membership$state, "active")
 
 })
 
@@ -214,13 +254,26 @@ test_that("view_membership returns a list of membership properties", {
 
 test_that("delete_membership removes users from an organization or team", {
 
-  team_membership <- delete_membership("ChadGoymer2", "HairyCoos", "HeadCoos")
+  skip_if(
+    length(user_2$login) != 1,
+    "Test user does not exist"
+  )
+  skip_if(
+    length(org$login) != 1,
+    "Authenticated user is not a member of an organization"
+  )
+  skip_if(
+    length(team$name) != 1,
+    "Organization does not have any teams defined"
+  )
+
+  team_membership <- delete_membership(user_2$login, org$login, team$name)
 
   expect_is(team_membership, "logical")
   expect_identical(attr(team_membership, "status"), 204L)
   expect_identical(as.logical(team_membership), TRUE)
 
-  org_membership <- delete_membership("ChadGoymer2", "HairyCoos")
+  org_membership <- delete_membership(user_2$login, org$login)
 
   expect_is(org_membership, "logical")
   expect_identical(attr(org_membership, "status"), 204L)
